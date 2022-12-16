9374a226e770dda122d9fdad4ad508fea617080e
Bracey Summers <bcsummers@gmail.com>
2022-12-16 10:15:23 -0600

    General cleanup - removed metadata file - updated pre-commit config - added codeql github action for security scanning

--- falcon_provider_logger/__init__.py
+++ falcon_provider_logger/__init__.py
@@ -1,12 +1,4 @@
 """Falcon logger module."""
 # flake8: noqa
-from .__metadata__ import (
-    __author__,
-    __author_email__,
-    __description__,
-    __license__,
-    __package_name__,
-    __url__,
-    __version__,
-)
-from .utils import rotating_handler, syslog_handler
+# first-party
+from falcon_provider_logger.utils import rotating_handler, syslog_handler
